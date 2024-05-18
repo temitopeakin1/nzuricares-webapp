18df4e06be8a673340452f95fbc7142e8b868b31
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-05-18 13:05:34 +0100

    update new

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,9 +1,5 @@
 import Signup from "@/Components/Signup";
 
 export default function SignupRoute() {
-  return <Signup onSignUpSuccess={function (isSuccess: boolean): void {
-      throw new Error("Function not implemented.");
-  } } onCancel={function (): void {
-      throw new Error("Function not implemented.");
-  } } />;
+  return <Signup />;
 }
