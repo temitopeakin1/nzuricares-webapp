1378b9344d0fe798e5d9c5765ce01c954ad42c9a
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-06-05 14:33:18 +0100

    update: careers page

--- app/company/careers/page.tsx
+++ app/company/careers/page.tsx
@@ -95,7 +95,7 @@ const Page = () => {
             </h1>
             <Link href={"/company/careers/#"} passHref>
           <button className="mt-8 px-[2em] py-[.5em] mx-24 bg-gradient-to-r from-blue-900 to-green-700 hover:bg-red-400 text-white rounded-full md:text-xl text-base duration-300 hover:scale-110 transform transition-all ease-in-out font-sans">
-            Check Open roles
+            Open roles
           </button>
         </Link>
           </FadeIn>
