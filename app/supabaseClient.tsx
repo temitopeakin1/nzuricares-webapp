9df79c30512cd233930a32a287409b848c5290a3
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-10-01 17:14:09 +0100

    update supabaseclient

--- app/supabaseClient.tsx
+++ app/supabaseClient.tsx
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing Supabase URL or Key environment variables");
