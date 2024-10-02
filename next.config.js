b2885cadfcdffabd67fa9f664335a1b51484de45
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-10-02 11:53:38 +0100

    update the nextconfig

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-            {
-            source: '/care-professionals',
-            destination: '/care-professionals/how-it-works',
-            permanent: true,
-            }
-        ]
+    env: {
+      NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+      POSTMARK_SERVER_TOKEN: process.env.POSTMARK_SERVER_TOKEN,
+      RESEND_API_KEY: process.env.RESEND_API_KEY,
     },
-};
-
-module.exports = nextConfig
-
-
-
-
+    async redirects() {
+      return [
+        {
+          source: '/care-professionals',
+          destination: '/care-professionals/how-it-works',
+          permanent: true,
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
+  
