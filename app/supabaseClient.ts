a220aa4292292fde01b28fbbff1c1639c4d5a0d5
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-10-03 16:25:32 +0100

    update env

--- app/supabaseClient.ts
+++ app/supabaseClient.ts
@@ -4,6 +4,8 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+console.log("Supabase URL:", supabaseUrl);
+console.log("Supabase Anon Key:", supabaseKey);
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing Supabase URL or Key environment variables");
--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
+  //console.log(process.env.NEXT_PUBLIC_SUPABASE_URL)
   const res = NextResponse.next();
   const supabase = createMiddlewareClient({ req, res });
   await supabase.auth.getSession();
