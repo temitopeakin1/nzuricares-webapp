594a72154b04256d7bdaa27003340393f7799f58
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-11-22 22:34:43 +0100

    update

--- app/terms-conditions/page.tsx
+++ app/terms-conditions/page.tsx
@@ -1,9 +1,336 @@
-import React from 'react'
+import Header from "@/Components/Header";
+import Footer from "@/Components/UI/Footer";
+import React from "react";
 
-const page = () => {
+const Page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <Header />
+      <div className="mx-8 md:mx-36 mt-28 md:mt-36">
+        <div className="text-xl md:text-4xl font-serif font-semibold text-blue-800 text-left">
+          Terms and Conditions
+        </div>
+        <p className="mt-2 text-justify text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          This page (together with the documents referred to on it) tells you
+          the terms of use on which you may make use of our website [
+          https://www.nzuricares.co.uk ]. <br />
+          Please read these terms of use carefully before you start to use the
+          site. By using our site, you indicate that you accept these terms of
+          use and that you agree to abide by them. If you do not agree to these
+          terms of use, please refrain from using our site.
+        </p>
+        <div className="mt-2 md:mt-4 text-[#283544] text-lg font-semibold font-title">
+          Information about us
+        </div>
+        <p className="mt-2 text-justify text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          <span className="font-bold">www.nzurihealthcare.co.uk</span> is a
+          website operated by Nzuri Healthcare Recruitment Limited.
+        </p>
+        <div className="mt-2 md:mt-4 text-[#283544] text-lg font-semibold font-title">
+          Accessing our site
+        </div>
+        <p className="mt-2 text-justify text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          Access to our site is permitted on a temporary basis, and we reserve
+          the right to withdraw our site without notice (see below). We will not
+          be liable if for any reason our site is unavailable at any time or for
+          any period. When using our site, you must comply with the provisions
+          of these terms. You are responsible for making all arrangements
+          necessary for you to have access to our site. You are also responsible
+          for ensuring that all persons who access our site through your
+          internet connection are aware of these terms and that they comply with
+          them.
+        </p>
+        <div className="mt-2 md:mt-4 text-[#283544] text-lg font-semibold font-title">
+          Intellectual Property Rights
+        </div>
+        <p className="mt-2 text-justify text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          We are the owner or the licensee of all intellectual property rights
+          in our site, and in the material published on it. Those works are
+          protected by copyright laws and treaties around the world. All such
+          rights are reserved. You may print and download extracts of any
+          page(s) from our site for your personal reference and you may draw the
+          attention of others within your organisation to material posted on our
+          site. You must not modify the paper or digital copies of any materials
+          you have printed off or downloaded in any way, and you must not use
+          any illustrations, photographs, video or audio sequences or any
+          graphics separately from any accompanying text. Our status (and that
+          of any identified contributors) as the authors of material on our site
+          must always be acknowledged. You must not use any part of the
+          materials on our site for commercial purposes without obtaining a
+          licence to do so from us or our licensors. If you print off, copy or
+          download any part of our site in breach of these terms of use, your
+          right to use our site will cease immediately and you must, at our
+          option, return or destroy any copies of the materials you have made.
+        </p>
+        <div className="mt-2 md:mt-4 text-[#283544] text-lg font-semibold font-title">
+          Reliance on Information Posted
+        </div>
+        <p className="mt-2 text-justify text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          Commentary and other materials posted on our site are not intended to
+          amount to advice on which reliance should be placed. We therefore
+          disclaim all liability and responsibility arising from any reliance
+          placed on such materials by any visitor to our site, or by anyone who
+          may be informed of any of its contents.
+        </p>
+        <div className="mt-2 md:mt-4 text-[#283544] text-lg font-semibold font-title">
+          Our Liability
+        </div>
+        <p className="mt-2 text-justify text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          The material displayed on our site is provided without any guarantees,
+          conditions, or warranties as to its accuracy. To the extent permitted
+          by law, we, other members of our group of companies, and third parties
+          connected to us hereby expressly exclude:
+        </p>
+        <ul className="text-justify list-disc list-inside ml-4 md:ml-6 text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          <li>
+            All conditions, warranties, and other terms which might otherwise be
+            implied by statute, common law, or the law of equity.
+          </li>
+          <li>
+            Any liability for any direct, indirect, or consequential loss or
+            damage incurred by any user in connection with our site or in
+            connection with the use, inability to use, or results of the use of
+            our site, any websites linked to it, and any materials posted on it,
+            including, without limitation any liability for:
+            <ul className="text-justify list-disc list-inside ml-4 md:ml-6 text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+              <li>Loss of income or revenue;</li>
+              <li>Loss of business;</li>
+              <li>Loss of profits or contracts;</li>
+              <li>Loss of anticipated savings;</li>
+              <li>Loss of data;</li>
+              <li>Loss of goodwill;</li>
+              <li>Wasted management or office time;</li>
+            </ul>
+          </li>
+        </ul>
+        <p className="mt-4 text-justify text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          This does not affect our liability for death or personal injury
+          arising from our negligence, nor our liability for fraudulent
+          misrepresentation or misrepresentation as to a fundamental matter, nor
+          any other liability which cannot be excluded or limited under
+          applicable law.
+        </p>
+        <div className="mt-2 md:mt-4 text-[#283544] text-lg font-semibold font-title">
+          Hacking and Viruses
+        </div>
+        <p className="mt-2 text-justify text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          You must not misuse our site by knowingly introducing viruses,
+          trojans, worms, logic bombs or other material which is malicious or
+          technologically harmful. You must not attempt to gain unauthorised
+          access to our site, the server on which our site is stored or any
+          server, computer or database connected to our site. You must not
+          attack our site via a denial-of-service attack or a distributed
+          denial-of service attack. By breaching this provision, you would
+          commit a criminal offence under the Computer Misuse Act 1990. We will
+          report any such breach to the relevant law enforcement authorities and
+          we will co-operate with those authorities by disclosing your identity
+          to them. In the event of such a breach, your right to use our site
+          will cease immediately. We will not be liable for any loss or damage
+          caused by a distributed denial-of-service attack, viruses or other
+          technologically harmful material that may infect your computer
+          equipment, computer programs, data or other proprietary material due
+          to your use of our site or to your downloading of any material posted
+          on it, or on any website linked to it.
+        </p>
+        <div className="mt-2 md:mt-4 text-[#283544] text-lg font-semibold font-title">
+          Linking to our site
+        </div>
+        <p className="mt-2 text-justify text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          You may link to our home page, provided you do so in a way that is
+          fair and legal and does not damage our reputation or take advantage of
+          it, but you must not establish a link in such a way as to suggest any
+          form of association, approval or endorsement on our part where none
+          exists. Our site must not be framed on any other site,. We reserve the
+          right to withdraw linking permission without notice. The website from
+          which you are linking must comply with applicable law in the UK and in
+          any country from which they are posted, and must not:
+        </p>
+        <ul className="text-justify list-disc list-inside ml-4 md:ml-6 text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          <li>Contain any material which is defamatory of any person</li>
 
-export default page
+          <li>
+            Contain any material which is obscene, offensive, hateful or
+            inflammatory
+          </li>
+          <li>Promote sexually explicit material</li>
+          <li>Promote violence</li>
+          <li>
+            Promote discrimination based on race, sex, religion, nationality,
+            disability, sexual orientation or age
+          </li>
+          <li>
+            Infringe any copyright, database right or trade mark of any other
+            person
+          </li>
+          <li>Be likely to deceive any person</li>
+          <li>
+            Be made in breach of any legal duty owed to a third party, such as a
+            contractual duty or a duty of confidence
+          </li>
+          <li> Promote any illegal activity</li>
+          <li>
+            Be threatening, abuse or invade another’s privacy, or cause
+            annoyance, inconvenience or needless anxiety
+          </li>
+          <li>
+            Be likely to harass, upset, embarrass, alarm or annoy any other
+            person
+          </li>
+          <li>
+            Be used to impersonate any person, or to misrepresent your identity
+            or affiliation with any person
+          </li>
+          <li>
+            Give the impression that they emanate from us, if this is not the
+            case; and{" "}
+          </li>
+          <li>
+            Advocate, promote or assist any unlawful act such as (by way of
+            example only) copyright infringement or computer misuse. If you wish
+            to make any use of material on our site other than that set out
+            above, please address your request to info@nzuricaresco.uk
+          </li>
+        </ul>
+        <div className="mt-2 md:mt-4 text-[#283544] text-lg font-semibold font-title">
+          Links from our site
+        </div>
+        <p className="mt-2 text-justify text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          Where our site contains links to other sites and resources provided by
+          third parties, these links are provided for your information only. We
+          have no control over the contents of those sites or resources and
+          accept no responsibility for them or for any loss or damage that may
+          arise from your use of them.
+        </p>
+        <div className="mt-2 md:mt-4 text-[#283544] text-lg font-semibold font-title ">
+          Prohibited Use
+        </div>
+        <p className="mt-2 text-justify text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          You may use our site only for lawful purposes. You may not use our
+          site:
+        </p>
+        <ul className="text-justify list-disc list-inside ml-4 md:ml-6 text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          <li>
+            In any way that breaches any applicable local, national or
+            international law or regulation
+          </li>
+          <li>
+            In any way that is unlawful or fraudulent, or has any unlawful or
+            fraudulent purpose or effect
+          </li>
+          <li>
+            To transmit, or procure the sending of, any unsolicited or
+            unauthorised advertising or promotional material or any other form
+            of similar solicitation (spam); or
+          </li>
+          <li>Promote violence</li>
+          <li>
+            To knowingly transmit any data, send or upload any material that
+            contains viruses, Trojan horses, worms, time-bombs, keystroke
+            loggers, spyware, adware or any other harmful programs or similar
+            computer code designed to adversely affect the operation of any
+            computer software or hardware. You also agree:
+          </li>
+          <li>
+            Not to reproduce, duplicate, copy or re-sell any part of our site in
+            contravention of the provisions of these terms.
+          </li>
+          <li>
+            Not to access without authority, interfere with, damage or disrupt:
+            • any part of our site;
+          </li>
+          <li>Any equipment or network on which our site is stored</li>
+          <li>Any software used in the provision of our site</li>
+          <li>
+            Any equipment or network or software owned or used by any third
+            party
+          </li>
+        </ul>
+        <div className="mt-2 md:mt-4 text-[#283544] text-lg font-semibold font-title ">
+          Suspension and Termination
+        </div>
+        <p className="mt-2 text-justify text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          We will determine, in our discretion, whether there has been a breach
+          of these terms through your use of our site. When a breach of this
+          policy has occurred, we may take such action as we deem appropriate.
+          Breach of these terms of use may result in our taking all or any of
+          the following actions:{" "}
+        </p>
+        <ul className="list-disc list-inside ml-4 md:ml-6 text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          <li>
+            Immediate, temporary or permanent withdrawal of your right to use
+            our site;
+          </li>
+          <li>Issue of a warning to you;</li>
+          <li>
+            To transmit, or procure the sending of, any unsolicited or
+            unauthorised advertising or promotional material or any other form
+            of similar solicitation (spam); or
+          </li>
+          <li>Promote violence</li>
+          <li>
+            To knowingly transmit any data, send or upload any material that
+            contains viruses, Trojan horses, worms, time-bombs, keystroke
+            loggers, spyware, adware or any other harmful programs or similar
+            computer code designed to adversely affect the operation of any
+            computer software or hardware. You also agree:
+          </li>
+          <li>
+            Legal proceedings against you for reimbursement of all costs on an
+            indemnity basis (including, but not limited to, reasonable
+            administrative and legal costs) resulting from the breach
+          </li>
+          <li>Further legal action against you; and/or</li>
+          <li>
+            Disclosure of such information to law enforcement authorities as we
+            reasonably feel is necessary. We exclude liability for actions taken
+            in response to breaches of these terms. The responses described in
+            these terms are not limited, and we may take any other action we
+            reasonably deem appropriate.
+          </li>
+        </ul>
+        <div className="mt-2 md:mt-4 text-[#283544] text-lg font-semibold font-title ">
+          Jurisdiction and Application Law
+        </div>
+        <p className="mt-4 text-justify text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          The English courts will have exclusive jurisdiction over any claim
+          arising from, or related to, a visit to our site although we retain
+          the right to bring proceedings against you for breach of these
+          conditions in your country of residence or any other relevant country.
+          These terms of use and any dispute or claim arising out of or in
+          connection with them or their subject matter or formation (including
+          non-contractual disputes or claims) shall be governed by and construed
+          in accordance with the law of England and Wales.
+        </p>
+        <div className="mt-2 md:mt-4 text-[#283544] text-lg font-semibold font-title ">
+          Trademarks
+        </div>
+        <p className="mt-4 text-justify text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          Nzuri Healthcare Recruitment Limited is a UK unregistered trade mark
+          of Nzuri Healthcare.
+        </p>
+        <div className="mt-2 md:mt-4 text-[#283544] text-lg font-semibold font-title ">
+          Variations
+        </div>
+        <p className="mt-4 text-justify text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          We may revise these terms of use at any time by amending this page.
+          You are expected to check this page from time to time to take notice
+          of any changes we made, as they are binding on you. Some of the
+          provisions contained in these terms of use may also be superseded by
+          provisions or notices published elsewhere on our site.
+        </p>
+        <div className="mt-2 md:mt-4 text-[#283544] text-lg font-semibold font-title ">
+          Your Concerns
+        </div>
+        <p className="mt-4 text-justify text-[#283544] text-md font-body font-normal leading-8 md:leading-10">
+          If you have any concerns about material which appears on our site,
+          please contact info@nzuricares.co.uk
+          <br />
+          Thank you for visiting our site.
+        </p>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Page;
