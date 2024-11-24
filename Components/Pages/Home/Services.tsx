0cf3d306b92b1817a08f965eff0cca1ad7f6c5a2
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-11-24 19:21:02 +0100

    update

--- Components/Pages/Home/Services.tsx
+++ Components/Pages/Home/Services.tsx
@@ -32,7 +32,7 @@ const Services = () => {
   return (
     <div className="py-8 md:py-16 text-center">
       <h1 className="mt-2 font-bold text-xl md:text-4xl  text-blue-800 font-title ">
-        Services
+        Our Services
       </h1>
       <div className="mt-8 flex flex-wrap justify-center">
         <ServicesItem
